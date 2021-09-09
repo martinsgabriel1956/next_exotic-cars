538242163e96f97f64a7c800c2ab6e238c48ccf1
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-09-09 16:48:19 -0300

    feat: setting project

--- styles/globals.tsx
+++ styles/globals.tsx
@@ -1,4 +1,7 @@
-html,
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  html,
 body {
   padding: 0;
   margin: 0;
@@ -14,3 +17,5 @@ a {
 * {
   box-sizing: border-box;
 }
+
+`;
