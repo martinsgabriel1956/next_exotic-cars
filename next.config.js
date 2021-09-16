37b936f00cf526bb17f7e7b5ba9546649a5b03cb
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-09-16 17:56:21 -0300

    feat: install axios and setting images domains

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: ['res.cloudinary.com', 'www.porschecenterbh.com.br', 'files.porsche.com', 'cdn.drivek.it', 'images.jazelc.com', 'www.lamborghini.com', 'cdn.imagin.studio', 'bluesky-cogcms.cdn.imgeng.in', 'www.lamborghini.com', 'www.webmotors.com.br', 'i.pinimg.com', 'pngimg.com', 'beta.alpes.one', 'production.autoforce.com', '/logospng.org', 'lh3.googleusercontent.com', 'cdn.freelogovectors.net'],
+  },
 }
